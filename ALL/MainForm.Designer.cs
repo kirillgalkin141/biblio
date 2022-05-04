9a1e0b0823ca345e467db636b531574977923770
zhernovnikov <nikita.zhernovnikov@inbox.ru>
2022-05-04 14:02:55 +0300

    fix1

--- ALL/MainForm.Designer.cs
+++ ALL/MainForm.Designer.cs
@@ -170,7 +170,7 @@ namespace _13.ALL
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(706, 450);
+            this.ClientSize = new System.Drawing.Size(706, 351);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.numericUpDown2);
             this.Controls.Add(this.label5);
